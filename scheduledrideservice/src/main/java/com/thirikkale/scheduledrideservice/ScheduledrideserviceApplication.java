e9e1cc8be449a7df0326456f5d99c49f378037f7
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2025-10-18 11:09:47 +0530

    inititalize project

---
+++ scheduledrideservice/src/main/java/com/thirikkale/scheduledrideservice/ScheduledrideserviceApplication.java
@@ -0,0 +1,13 @@
+package com.thirikkale.scheduledrideservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledrideserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledrideserviceApplication.class, args);
+	}
+
+}
